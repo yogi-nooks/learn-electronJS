d959068639b6171cca56da6a1a4ee9aa3c3e9969
yogesh-nooks <yogesh.nooks@gmail.com>
2018-06-25 15:18:54 +0530

    chngs in main.js

--- main.js
+++ main.js
@@ -16,11 +16,17 @@ bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
 
 let mainWindow
 
-function createWindow () {
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+
+app.on('ready', function(e) {
   // Create the browser window.
+  console.log(e);
   mainWindow = new BrowserWindow({width: 1400, height: 600})
   
-
+  console.log('app is ready');
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
   
@@ -35,12 +41,14 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
-}
+})
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+//Before quit 
+
+app.on('will-finish-launching', function(){
+  console.log(10+90);
+});
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
